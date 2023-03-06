f95fc5a9cd09a96a3cf90190034c217becdb4158
Kento Puleo <kento.puleo@pax.ch>
2023-03-06 09:47:35 +0100

    CanActive import error (TS2395: Individual declarations in merged declaration 'CanActivate' must be all exported or all local.)

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,13 @@
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-
+import {AuthState} from "./reducers";
+import {Store} from "@ngrx/store";
 
 @Injectable()
 export const AuthGuard implements CanActivate{
 
-  constructorChecks(private store. Store<AuthState>)
+  constructor(private store: Store<AuthState>)
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
   Observable<boolean> {
